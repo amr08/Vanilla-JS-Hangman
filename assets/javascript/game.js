8cccede0c41bb6890a028f5f79ab4b49304c2f60
Andrea R <amrdev08@gmail.com>
2016-08-05 10:14:43 -0400

    updated code/pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,99 @@
-//Theme "Surf"
+var word = ["beachboys", "311"];
+var random = Math.floor(Math.random() *2)
+var actualLetters = word[random];
+var length = actualLetters.length;
+var hidden = [length];
+var guessesLeft = 12;
+var correct = length;
+var output = " ";
+var letters = actualLetters.split ('');
+var losses = [];
+var totalWins= [];
+var list = [];
 
-var guessesLeft= 12;
-var correct = 0;
-var losses = 0;
-var totalWins = 0;
 
-//Hangman Choices
 
-var answerBoys = ["b","e","a","c","h","b","o","y","s"];
-//var anotherGame = ["a","n","o""....]
-var secret = [];
 
-//placeholder grabber
-    for(var i = 0; i < answerBoys.length; i++) {
-      secret.push(" _ ");
-      
-     var secretAnswer = document.getElementById("secretAnswer").innerHTML = secret;
+
+function gameStart() {
+    for(var i = 0; i < actualLetters.length; i++) {
+    hidden[i] = "_ ";
+    output = output + hidden[i];
+   
             
     }
+        document.getElementById("secretAnswer").innerHTML = output;
+    output = " ";
 
-//userAnswer grabber
-    document.onkeypress = function(event) {
+
+}
+
+gameStart();
+
+
+
+ function user() { 
+
+document.onkeypress = function(event) {
 
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-  
+  var music = userGuess + word;
 
-    if (answerBoys.indexOf(userGuess) == -1) {
-    guessesLeft--;
-    var list = userGuess;
+for(var j = 0; j < actualLetters.length; j++) {
 
-     document.getElementById("answersGuessed").innerHTML = list;
-     document.getElementById("guessesLeft").innerHTML = guessesLeft;
-    }
-  
-  else {
-    correct++
-    var reveal = secret.pop(userGuess);
-
-    document.getElementById("secretAnswer").innerHTML = reveal;
-      }
-    
-//results if wins or looses
-    if (correct === answerBoys.length) {
-                        totalWins++;
-                        document.getElementById("game").innerHTML = "<p> TotalWins</p>" + totalWins;
-                        var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia801408.us.archive.org/1/items/TheBeachBoys-GreatestHits/04%20Surfin%27%20U.S.A..mp3'></audio>"   
-                        document.querySelector("#audio").innerHTML = audio;  
-                        var picture = "<img src='https://raw.githubusercontent.com/amr08/week-3-game/master/assets/images/onprinciple-beachboys-surfergirl-500.jpg'>"
-                        document.querySelector("#picture").innerHTML = picture;  
- 
-                          }
-
-                          else if (guessesLeft === 0) {
-                            losses++;
-                            document.getElementById("losses").innerHTML =  losses;
-                            //how to restart game!?!!?!?!?!?!
-                          }
    
+
+  if (userGuess == letters[j]) {
+     
+     hidden[j] = userGuess;
+     correct--;
+
+     }
+
+     else {
+        output = output + hidden[j] + " ";
+     }
+
+}
+list.push(userGuess);
+    document.getElementById("secretAnswer").innerHTML = output;
+    document.getElementById("answersGuessed").innerHTML = list;
+    
+    output = " ";
+    guessesLeft--;
+
+     
+     if (correct < 1) {
+        totalWins++;
+         if ( word ===)
+        document.getElementById("game").innerHTML =  totalWins;
+        var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia801408.us.archive.org/1/items/TheBeachBoys-GreatestHits/04%20Surfin%27%20U.S.A..mp3'></audio>"   
+        document.querySelector("#audio").innerHTML = audio;  
+        var picture = "<img src='https://raw.githubusercontent.com/amr08/week-3-game/master/assets/images/onprinciple-beachboys-surfergirl-500.jpg'>"
+        document.querySelector("#picture").innerHTML = picture;  
+ 
+                    }
+
+
+    else if (guessesLeft === 0) {
+        losses++;
+        document.getElementById("losses").innerHTML =  losses;
+                            //how to restart game!?!!?!?!?!?!
+       }
+
+    else {
+
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
+
+
     }
-      
+
+}
+
+}
+  
+  user();
+
 
 
 
