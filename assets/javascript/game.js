687bc90953b9907ac6c9043cd05475b02328c9ff
Andrea R <amrdev08@gmail.com>
2016-08-06 16:29:53 -0400

    picture

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-var word = ["beachboys", "jackjohnson", "dickdale", "311", "sublime"];
-var random = Math.floor(Math.random() * 4)
+var word = ["beachboys", "jackjohnson", "dickdale", "311", "bobmarley"];
+var random = Math.floor(Math.random() * 5)
 var actualLetters = word[random];
 var length = actualLetters.length;  
 var hidden = [];
@@ -9,8 +9,10 @@ var output = "";
 //var letters = actualLetters.split ('');
 
 var losses = 0;
-var totalWins= 0;
+var wins= 0;
 var list = [];
+var totalWins = 0;
+
 
 
 
@@ -72,31 +74,34 @@ if(actualLetters.indexOf(userGuess) == - 1) {
  output = " ";
 
             if (correct <= 0 && guessesLeft > 0 && actualLetters == "beachboys") {
-                totalWins++;
+                wins++;
          
-                        document.getElementById("game").innerHTML =  totalWins;
-                        var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia801408.us.archive.org/1/items/TheBeachBoys-GreatestHits/04%20Surfin%27%20U.S.A..mp3'></audio>"   
+                        document.getElementById("game").innerHTML =  wins;
+                        var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia800202.us.archive.org/27/items/14TheBeachBoysTheWarmthOfTheSun/The%20Beach%20Boys%20-%20Good%20Vibrations.mp3'></audio>"   
                         document.querySelector("#audio").innerHTML = audio;  
                         var picture = "<img src='https://raw.githubusercontent.com/amr08/week-3-game/master/assets/images/onprinciple-beachboys-surfergirl-500.jpg'>"
                         document.querySelector("#picture").innerHTML = picture; 
-    
-                    } 
+                       
+  
+                       }
 
+                    
     
-            if (correct <= 0 && guessesLeft > 0 && actualLetters == "jackjohnson") {
-                totalWins++;
+            else if (correct <= 0 && guessesLeft > 0 && actualLetters == "jackjohnson") {
+                wins++;
          
-                        document.getElementById("game").innerHTML =  totalWins;
+                        document.getElementById("game").innerHTML =  wins;
                         var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia601008.us.archive.org/31/items/JackJohnsonSittingWaitingWishingToqueMP3/jack%20johnson%20banana%20pancakes%20-%20[Toque%20MP3].mp3'></audio>"   
                         document.querySelector("#audio").innerHTML = audio;  
                         var picture = "<img src='https://raw.githubusercontent.com/amr08/week-3-game/master/assets/images/800beb7840472ea335bcd4dfe59d4fca.jpg'>"
                         document.querySelector("#picture").innerHTML = picture;  
-                            
-                    }          
-             if (correct <= 0 && guessesLeft > 0 && actualLetters == "dickdale") {
-                totalWins++;
+                         
+                    }   
+
+             else if (correct <= 0 && guessesLeft > 0 && actualLetters == "dickdale") {
+                wins++;
          
-                        document.getElementById("game").innerHTML =  totalWins;
+                        document.getElementById("game").innerHTML =  wins;
                         var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia801606.us.archive.org/31/items/DickDaleAndTheDeltones-01-50/DickDaleAndTheDel-tones-Misirlou.mp3'></audio>"   
                         document.querySelector("#audio").innerHTML = audio;  
                         var picture = "<img src='https://raw.githubusercontent.com/amr08/week-3-game/master/assets/images/216931.jpg'>"
@@ -105,35 +110,37 @@ if(actualLetters.indexOf(userGuess) == - 1) {
                     }
                    
 
-             if (correct <= 0 && guessesLeft > 0 && actualLetters == "311") {
-                totalWins++;
+             else if (correct <= 1 && guessesLeft > 0 && actualLetters == "311") {
+                wins++;
          
-                        document.getElementById("game").innerHTML =  totalWins;
+                        document.getElementById("game").innerHTML =  wins++;
                         var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia601008.us.archive.org/18/items/311IllBeHereAwhile/311%20-%20I%27ll%20Be%20Here%20Awhile.mp3'></audio>"   
                         document.querySelector("#audio").innerHTML = audio;  
-                        var picture = "<img src='https://raw.githubusercontent.com/amr08/week-3-game/master/assets/images/beautiful-beach-wide-hd-wallpaper-for-desktop-background-download-beach-images-free%20copy.jpg'>"
+                        var picture = "<img src='https://raw.githubusercontent.com/amr08/week-3-game/master/assets/images/311thumb2_400x400.jpg'>"
                         document.querySelector("#picture").innerHTML = picture; 
-                              
+                           
                     }
 
-            if (correct <= 0 && guessesLeft > 0 && actualLetters == "sublime") {
-                totalWins++;
+            else if (correct <= 0 && guessesLeft > 0 && actualLetters == "bobmarley") {
+                wins++;
          
-                        document.getElementById("game").innerHTML =  totalWins;
-                        var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia601008.us.archive.org/18/items/311IllBeHereAwhile/311%20-%20I%27ll%20Be%20Here%20Awhile.mp3'></audio>"   
+                        document.getElementById("game").innerHTML =  wins++;
+                        var audio = "<audio autoplay = 'autoplay' >" + "<source src='https://ia802605.us.archive.org/9/items/BobMarley_201410/Bob%20Marley%20-%20DISCOGRAFIA/Bob%20Marley-Legend/15%20Jamming.mp3'></audio>"   
                         document.querySelector("#audio").innerHTML = audio;  
                         var picture = "<img src=''>"
                         document.querySelector("#picture").innerHTML = picture; 
-                              
+                         
                     }
+
             else if (guessesLeft < 0) {
                         losses++;
                         location.reload ();
         
                     }
          //How to count wins without reload
- 
+        
 };
+ 
 
 //end
 
